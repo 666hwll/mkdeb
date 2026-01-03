204960e1dab63490aa9dd58043e2fde66cece607
666hwll <darwin.zmugg@mail.de>
2026-01-03 20:03:08 +0100

    major overhaul; including newly structured cpp version

--- README.md
+++ README.md
@@ -1,6 +1,12 @@
 # mkdeb
- a bash script for making deb-files easier
-install it simply with:
+---------------------------------------------------------------
+A bash script for making .deb-archives easier to build.
+You can install it on Linux, by copying this one-liner into your terminal-emulator:
 ``` bash
 curl https://raw.githubusercontent.com/666hwll/mkdeb/main/mkdeb >> mkdeb && chmod +x mkdeb && sudo mv mkdeb /usr/local/bin/
+```
+
+## There is also a newer C++-Version, if you are open to test it.
+``` bash
+curl https://raw.githubusercontent.com/666hwll/mkdeb/main/mkdeb.cpp >> mkdeb.cpp && g++ mkdeb.cpp -o mkdeb && sudo mv mkdeb /usr/local/bin/
 ```
--- mkdeb.cpp
+++ mkdeb.cpp
@@ -3,65 +3,82 @@
 #include <string>
 #include <filesystem>
 
-
-int main()
-{   
-    
+struct package {
     std::string projectname = "";
-    std::string arch = "";
+    std::string cpu_architecture = "";
+    std::string maintainer = "";
     std::string description = "";
     std::string executionHandle = "";
     std::string path_to_icon = "";
     unsigned int IsTerminalonly = 0;
     unsigned int IsNotifiengatStartup = 0;
-    std::string boolOptions[] = {"false", "false"};
+    std::string boolOptions[2] = {"false", "false"};
     std::string version_number = "0.0.1";
-    std::string categories_for_desktop[12] = {"AudioVideo", "Audio", "Video", "Development", "Education", "Game", "Graphics", "Network", "Office", "Science", "Settings", "System", "Utility"};
+    std::string categories_for_desktop[13] = {"AudioVideo", "Audio", "Video", "Development", "Education", "Game", "Graphics", "Network", "Office", "Science", "Settings", "System", "Utility"};
     int user_index_for_categories_for_desktop = -1;
+};
+
+int main()
+{   
+    package p1;
 
     std::cout << "\nWhat is the name of the project? ";
-    std::cin >> projectname;
+    std::cin >> p1.projectname;
     std::cin.ignore();
 
-    std::cout << "\nWhat architecture is the project utilizing? ";
-    std::cin >> arch;
+    std::cout << "\nWhat cpu-architecture is the project utilizing? ";
+    std::cin >> p1.cpu_architecture;
+    std::cin.ignore();
+
+    std::cout << "\n Who is maintaining this project? ";
+    std::cin >> p1.maintainer;
     std::cin.ignore();
 
     std::cout << "\nHow would you describe the project? ";
-    std::getline(std::cin, description);
+    std::getline(std::cin, p1.description);
     std::cin.ignore();
 
     std::cout << "\nWhat is the version number, you want to utilize? ";
-    std::getline(std::cin, version_number);
+    std::getline(std::cin, p1.version_number);
     std::cin.ignore();
 
     std::cout << "\nWhat do you type in to execute the programm? ";
-    std::cin >> executionHandle;
+    std::cin >> p1.executionHandle;
     std::cin.ignore();
 
-    std::cout << "\nType in the path to the icon? If you don't need an icon, type in \'system\' ";
-    std::cin >> path_to_icon;
+    std::cout << "\nType in the path to the icon? If you don't need an icon, type in \'0\' ";
+    std::cin >> p1.path_to_icon;
+    if(p1.path_to_icon == "0") {
+        p1.path_to_icon = "system";
+    }
     std::cin.ignore();
 
     std::cout << "\nIs it supposed to run in the terminal (1/0)? ";
-    std::cin >> IsTerminalonly;
+    std::cin >> p1.IsTerminalonly;
     std::cin.ignore();
-    if(IsTerminalonly == 1) {
-        boolOptions[0] = "true";
+    if(p1.IsTerminalonly == 1) {
+        p1.boolOptions[0] = "true";
     }
 
     std::cout << "\nNotify at startup (1/0)? ";
-    std::cin >> IsNotifiengatStartup;
+    std::cin >> p1.IsNotifiengatStartup;
     std::cin.ignore();
-    if(IsNotifiengatStartup == 1) {
-        boolOptions[1] = "true";
+    if(p1.IsNotifiengatStartup == 1) {
+        p1.boolOptions[1] = "true";
     }
 
-    std::cout << "In which of these categories is your programm? Type in the number of the element for your choice\n" << categories_for_desktop << "\n >>";
-        do {
-            std::cin >> user_index_for_categories_for_desktop;
-        } while(-1 < (user_index_for_categories_for_desktop + 1) <= 13);
+    std::cout << "In which of these categories is your programm? Type in the number of the element for your choice\n";
+    for(int i = 0; i < 13; i++) {
 
+        std::cout << i << " . = " << p1.categories_for_desktop[i] << "\n";
+    }
+    std::cout << ">>\t";
+    do    
+    {
+        std::cin >> p1.user_index_for_categories_for_desktop;
+    } while(p1.user_index_for_categories_for_desktop < -1 || p1.user_index_for_categories_for_desktop > 13);
+
+    std::cout << "Done. Now the directories are being created ...\n";
 
     std::filesystem::create_directories("debianrel");
     std::filesystem::create_directory("debianrel/DEBIAN");
@@ -70,14 +87,14 @@ int main()
     std::filesystem::create_directory("debianrel/usr/share");
     std::filesystem::create_directory("debianrel/usr/share/applications");
 
-
+    std::cout << "Finished.\n";
 
     std::ofstream ControlFile("debianrel/DEBIAN/control");
-    ControlFile << "Package: " << projectname << "\nVersion: " << version_number << "\nArchitecture: " << arch << "\nMaintainer: " << "$USER" << "\nDescription: " << description;
+    ControlFile << "Package: " << p1.projectname << "\nVersion: " << p1.version_number << "\nArchitecture: " << p1.cpu_architecture << "\nMaintainer: " << p1.maintainer << "\nDescription: " << p1.description;
     ControlFile.close();
 
-    std::ofstream DesktopFile("debianrel/usr/share/applications/" + projectname + ".desktop");
-    DesktopFile << "[Desktop Entry]\nEncoding=UTF-8\nName=" << projectname <<"\nComment=" << description << "\nExec=" << executionHandle << "\nIcon= " << path_to_icon <<"\nTerminal=" << boolOptions[0] <<"\nStartupNotify=" << boolOptions[1] <<"\nType=Application\nCategories= " << categories_for_desktop[user_index_for_categories_for_desktop - 1];
+    std::ofstream DesktopFile("debianrel/usr/share/applications/" + p1.projectname + ".desktop");
+    DesktopFile << "[Desktop Entry]\nEncoding=UTF-8\nName=" << p1.projectname <<"\nComment=" << p1.description << "\nExec=" << p1.executionHandle << "\nIcon=" << p1.path_to_icon <<"\nTerminal=" << p1.boolOptions[0] <<"\nStartupNotify=" << p1.boolOptions[1] <<"\nType=Application\nCategories=" << p1.categories_for_desktop[p1.user_index_for_categories_for_desktop];
     DesktopFile.close();
 
 }
